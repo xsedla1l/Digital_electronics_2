01c5b79de3f2e635b057614a12b6c8ef957979a8
student <student@PC-638.urel.feec.vutbr.cz>
2021-09-29 12:49:38 +0200

    button

--- Labs/02-leds/02-leds/02-leds/main.c
+++ Labs/02-leds/02-leds/02-leds/main.c
@@ -12,6 +12,7 @@
 /* Defines -----------------------------------------------------------*/
 #define LED_GREEN   PB5     // AVR pin where green LED is connected
 #define LED_RED     PC0     // AVR pin where red LED is connected
+#define PUSH_BTN     PD4     // AVR pin where push button
 #define BLINK_DELAY 200
 #ifndef F_CPU
 # define F_CPU 16000000     // CPU frequency in Hz required for delay
@@ -36,21 +37,28 @@ int main(void)
     PORTB = PORTB & ~(1<<LED_GREEN);
 
     // Configure the second LED at port C
-    DDRC = DDRC | (1<<LED_RED); // posun jednotku o 5 - ostatne sa nemenia!
-    PORTC = PORTC & ~(1<<LED_RED); // andujem 1 a na mojej poyicie je 0 - ostatne sa nemenia! 
+    //DDRC = DDRC | (1<<LED_RED); // posun jednotku o 5 - ostatne sa nemenia!
+    //PORTC = PORTC & ~(1<<LED_RED); // andujem 1 a na mojej poyicie je 0 - ostatne sa nemenia! 
 
     // Configure Push button at port D and enable internal pull-up resistor
-
+     DDRD = DDRD | (0<<PUSH_BTN);
+     
+     PORTD = PORTD & ~(0<<PUSH_BTN);
 
     // Infinite loop
     while (1)
     {
         // Pause several milliseconds
-        _delay_ms(BLINK_DELAY);
-        PORTB = PORTB ^ (1<<LED_GREEN);	 
-        PORTC = PORTC ^ (1<<LED_RED);
-        _delay_ms(BLINK_DELAY);
+        //_delay_ms(BLINK_DELAY);
+        //PORTB = PORTB ^ (1<<LED_GREEN);	 
+        //PORTC = PORTC ^ (1<<LED_RED);
+        //_delay_ms(BLINK_DELAY);
+            if(bit_is_clear(PIND4, PD4))
+            {
+                PORTC = PORTC & ~(1<<LED_RED); 
+            }
         
+           
     }
 
     // Will never reach this
