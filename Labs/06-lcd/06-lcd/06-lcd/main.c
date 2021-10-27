3d7babd80f29503dc34b4bb8ae4075d0b11351ae
Your Name <you@example.com>
2021-10-27 12:48:20 +0200

    labs

--- Labs/06-lcd/06-lcd/06-lcd/main.c
+++ Labs/06-lcd/06-lcd/06-lcd/main.c
@@ -23,15 +23,58 @@
  *           Timer/Counter2 overflows.
  * Returns:  none
  **********************************************************************/
+
+     uint8_t customChar[16] = {
+         0b00000,
+         0b01110,
+         0b11111,
+         0b11111,
+         0b11111,
+         0b01111,
+         0b00011,
+         0b00001,
+         0b00000,
+         0b01110,
+         0b11111,
+         0b11111,
+         0b11111,
+         0b11110,
+         0b11000,
+         0b10000
+     };
 int main(void)
 {
     // Initialize LCD display
-    lcd_init(LCD_DISP_ON);
+    lcd_init(LCD_DISP_ON_CURSOR);
 
     // Put string(s) at LCD display
-    lcd_gotoxy(1, 0);
-    lcd_puts("LCD Test");
-    lcd_putc('!');
+    lcd_gotoxy(0, 1);
+   // lcd_puts("Let's go to berlin");
+    // Initialize LCD display
+    lcd_init(LCD_DISP_ON);
+
+    // Set pointer to beginning of CGRAM memory
+    lcd_command(1 << LCD_CGRAM);
+    for (uint8_t i = 0; i <16; i++)
+    {
+        // Store all new chars to memory line by line
+        lcd_data(customChar[i]);
+    }
+    // Set DDRAM address
+    lcd_command(1 << LCD_DDRAM);
+    
+    // Display first custom character
+    lcd_putc(0);
+    lcd_putc(1);
+    //
+    
+    
+
+     
+  //  lcd_putc('!');
+   TIM2_overflow_16ms();
+   TIM2_overflow_interrupt_enable();
+    
 
     // Configure 8-bit Timer/Counter2 for Stopwatch
     // Set the overflow prescaler to 16 ms and enable interrupt
@@ -58,8 +101,15 @@ int main(void)
  *           ie approximately every 100 ms (6 x 16 ms = 100 ms).
  **********************************************************************/
 ISR(TIMER2_OVF_vect)
-{
+ {   
+     //static uint8_t a = 1;
+//     static uint8_t b = 0;
+//     static uint8_t c = 0;
+    
     static uint8_t number_of_overflows = 0;
+    static uint8_t tens = 0;        // Tenths of a second
+    static uint8_t secs = 0;        // Seconds
+    char lcd_string[2] = "  ";      // String for converting numbers by itoa()
 
     number_of_overflows++;
     if (number_of_overflows >= 6)
@@ -68,7 +118,36 @@ ISR(TIMER2_OVF_vect)
         number_of_overflows = 0;
 
         // WRITE YOUR CODE HERE
-
-    }
+     tens++;
+    if (tens>=10){
+         tens=0;
+         /*a = a*2;*/
+         secs++;
+         if (secs>=60){
+             secs=0;
+                      }        
+                      // works wonky with 59 sec
+                 }
+                 // display tenth of seconds
+                     lcd_gotoxy(1, 0);
+                     //lcd_puts("00:");
+                     //lcd_gotoxy(11, 0);
+                     //itoa(a, lcd_string, 10);
+                     lcd_gotoxy(6, 0);
+                     lcd_puts(".");
+                     itoa(tens, lcd_string, 10);     // Convert decimal value to string
+                     lcd_gotoxy(7,0);
+                     lcd_puts(lcd_string);
+                      // display seconds
+                      itoa(secs, lcd_string, 10);     // Convert decimal value to string
+                      if (secs<10){
+                      lcd_gotoxy(5,0);
+                      }
+                      else {        lcd_gotoxy(4,0);}
+                    
+                      lcd_puts(lcd_string);
+                   
+    }                
     // Else do nothing and exit the ISR
 }
+    
